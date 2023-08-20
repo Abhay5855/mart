9857087efcea7f123fc260654a171db0ac1e092d
pushpendu <pushpendu.ghosh@i2itherapy.com>
2023-08-20 17:32:39 +0530

    feat:connect databse and specify middlewares.

--- app.js
+++ app.js
@@ -2,12 +2,33 @@ require("dotenv").config();
 
 const express = require("express");
 
-const moongoose = require("mongoose");
-
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+var cors = require("cors");
 const app = express();
 
 const port = 3000;
 
+// Connect to the database
+mongoose
+  .connect("mongodb://127.0.0.1:27017/product", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("DB CONNECTED");
+  })
+  .catch((err) => {
+    console.log(err, "err");
+  });
+
+//Common Middlewares
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Start the servers
 
