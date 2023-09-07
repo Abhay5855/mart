301f8460c3c8c45fc3a166ccbbf08a3a6ea604d8
pushpendu <pushpendu.ghosh@i2itherapy.com>
2023-09-07 15:33:44 +0530

    feat:Create middleware to check if user is SignedIn and authenticated.

---
+++ middlewares/authenticate.js
@@ -0,0 +1,20 @@
+const expressJwt = require("express-jwt");
+
+exports.isAuthenticated = (req, res, next) => {
+  let checker = req.profile && req.auth && req.profile._id === req.auth._id;
+
+  if (!checker) {
+    return res.status(403).json({
+      error: "Access Denied",
+    });
+  }
+
+  next();
+};
+
+exports.isSignedIn = expressJwt({
+  secret: "shhhhh",
+  userProperty: "auth",
+});
+
+
